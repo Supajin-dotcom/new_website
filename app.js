632bd0fe1f96a97bfeb268843967c01b0e1abcc7
Supajin-dotcom <m.merveille.emanuel@gmail.com>
2026-05-02 18:10:59 -0400

    Update product URL to ngrok link

--- app.js
+++ app.js
@@ -293,7 +293,7 @@ const addToSnipcart = () => {
     quantity: formState.quantity,
     image: product.images[0],
     description: product.description[0],
-    url: '/',
+    url: 'https://plank-gestate-hatching.ngrok-free.dev',
     customFields: [
       { 
         name: "Texture",
